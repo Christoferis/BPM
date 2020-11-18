e14a4decc842a554a22f37a8d14955939b7bbefa
Christoferis <dale.breuer@gmx.de>
2020-11-18 16:23:16 +0100

    working on BPM Ticker

--- BPM.py
+++ BPM.py
@@ -1,7 +1,8 @@
 #Note: COmmit, implement vars correctly
 
 #Imports
-from tkinter import Tk, Label, Button, IntVar
+from tkinter import Tk, Label, Button, Frame
+from tkinter.ttk import Notebook
 from statistics import median
 from time import time
 
@@ -15,7 +16,7 @@ root = Tk()
 root.title("Tap BPM")
 root.geometry("300x150")
 
-#callback for time
+#BPM Functions
 def BPM(bpmtxt):
     global starttime, endtime, pastbpm
     #Stop the clock
@@ -37,17 +38,43 @@ def BPM(bpmtxt):
     #restart clock
     starttime = time()
 
+def BPM_GUI(tabs):
+    #make frame for easier stuff
+    bpm_frame = Frame(tabs)
+    #text
+    Label(bpm_frame, text="Press the Button to tap BPM").pack()
+    #Button and BPM Label
+    bpm = Label(bpm_frame, background="grey", foreground="white", text="Tap to start", pady=5)
+    but = Button(bpm_frame, text="Press me", command=lambda: BPM(bpm), height=2, width=8, background="green", foreground="white",)
+    bpm.pack()
+    but.pack()
+
+    return bpm_frame
+
+
+#Tick functions
+
+def Tick_GUI(tabs):
+    #Frame cuz easier
+    tick_frame = Frame(tabs)
+
+    
+
+    return tick_frame
+
 
 #main GUI
 def GUI():
+    #Main Notebook
     global root
-    #text
-    Label(root, text="Press the Button to tap BPM").pack()
-    #Button and BPM Label
-    bpm = Label(root, background="grey", foreground="white", text="Tap to start", pady=5)
-    but = Button(root, text="Press me", command=lambda: BPM(bpm), height=4, width=8, background="green", foreground="white")
-    but.pack()
-    bpm.pack()
+    tabs = Notebook(root)
+
+    #Add Tabs
+    tabs.add(BPM_GUI(tabs))
+
+    tabs.pack(fill="both", expand=1)
+
+
 
 #run GUI
 GUI()
